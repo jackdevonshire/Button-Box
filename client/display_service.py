06eca6c22e9c1f0a98e3b667d9180a0e1e562b4f
Jack Devonshire <itunes.jd@hotmail.com>
2024-06-09 21:35:18 +0100

    In client ignoring default screen message update if it is None

--- client/display_service.py
+++ client/display_service.py
@@ -37,13 +37,16 @@ class DisplayService():
             self.__lcd.printline(3, lines[3].rjust(cols))
 
     def set_default_message(self, default_message):
+        if default_message == None:
+            return
+
         self.__default_lines = default_message
 
     def set_temporary_message(self, message, duration):
         self.__set_lcd(message)
 
         if duration is None: # Do not switch back to default message until instructed if duration not set
-            return True
+            return
 
         time.sleep(duration)
         self.__set_lcd(self.__default_lines)
