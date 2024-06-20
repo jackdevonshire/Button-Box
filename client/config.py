b32771ebd75d4f5b0c41fc04cb13c9d61ecfb8f5
Jack Devonshire <itunes.jd@hotmail.com>
2024-06-20 20:24:11 +0100

    Updating button handling logic
    It appears the existing logic always believes the button pressed is the last one setup (protected 4) when calling the lambda. Changing the logic to account for this error unless I find a better way. Also correctly calling handle_event with the event in capitals, not regular casing.

--- client/config.py
+++ client/config.py
@@ -1,3 +1,5 @@
+from gpiozero import Button
+
 # The Local IP Address of the machine hosting the Python Flask Server
 HOST_IP = ""
 
@@ -6,23 +8,23 @@ AUTH_TOKEN = ""
 
 # All the buttons. The key is the button reference (customisable), the value is the GPIO pin for that button
 BUTTONS = {
-    "BTN_1": 14,
-    "BTN_2": 15,
-    "BTN_3": 18,
-    "BTN_4": 23,
-    "BTN_5": 24,
-    "BTN_6": 25,
-    "BTN_7": 8,
-    "BTN_8": 7,
-    "BTN_9": 12,
-    "BTN_10": 16,
-    "SWITCH_1": 17,
-    "SWITCH_2": 27,
-    "SWITCH_3": 22,
-    "SWITCH_4": 10,
-    "SWITCH_5": 9,
-    "PROTECTED_1": 5,
-    "PROTECTED_2": 6,
-    "PROTECTED_3": 13,
-    "PROTECTED_4": 19
+    "BTN_1": Button(14),
+    "BTN_2": Button(15),
+    "BTN_3": Button(18),
+    "BTN_4": Button(23),
+    "BTN_5": Button(24),
+    "BTN_6": Button(25),
+    "BTN_7": Button(8),
+    "BTN_8": Button(7),
+    "BTN_9": Button(12),
+    "BTN_10": Button(16),
+    "SWITCH_1": Button(17),
+    "SWITCH_2": Button(27),
+    "SWITCH_3": Button(22),
+    "SWITCH_4": Button(10),
+    "SWITCH_5": Button(9),
+    "PROTECTED_1": Button(5),
+    "PROTECTED_2": Button(6),
+    "PROTECTED_3": Button(13),
+    "PROTECTED_4": Button(19)
 }
--- client/main.py
+++ client/main.py
@@ -3,25 +3,34 @@
 # In Raspberry Pi preferences, enable the I2C interface
 #
 
-from display_service import DisplayService
 from client import Client
 from config import HOST_IP, AUTH_TOKEN, BUTTONS
-from gpiozero import Button
-from signal import pause
+from display_service import DisplayService
 
 display = DisplayService()
 client = Client(HOST_IP, AUTH_TOKEN, display)
 
-functional_buttons = {}
-
 client.setup()
 
-# Initial Setup
-for button_reference, button_gpio in BUTTONS.items():
-    functional_button = Button(button_gpio)
-    functional_buttons[button_reference] = functional_button
+# Initialise button states
+button_states = {}
+for button_reference, button in BUTTONS.items():
 
-    functional_button.when_pressed = lambda: client.handle_event("On", button_reference)
-    functional_button.when_released = lambda: client.handle_event("Off", button_reference)
+while True:
+    for button_reference, button in BUTTONS.items():
+        try:
+            is_pressed = button.is_pressed()
+            existing_is_pressed_state = button_states[button_reference]
+
+            if is_pressed != existing_is_pressed_state:
+                if is_pressed:
+                    client.handle_event("ON", button_reference)
+                else:
+                    client.handle_event("OFF", button_reference)
+
+                button_states[button_reference] = is_pressed
+
+        except:
+            continue
 
 pause()
