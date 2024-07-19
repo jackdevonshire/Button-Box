090eb24b977e98720f8ae1fd798f2aa838ee150c
Jack Devonshire <itunes.jd@hotmail.com>
2024-07-20 00:28:12 +0100

    Hopefully fixing mangled display
    I am trying to fix an issue whereby when pressing buttons abnormally fast, the display sometimes gets into an unrecoverable state whereby the text displays backwards. I theorise that this is due to the async nature of the flask API that updates the display, sometimes causing overlapping requests and as such potentially breaking the cursor position.
    
    Previously I tried to clear the display and reset the cursor position each display update, but this takes too long. Now I've introduced a variable to ensure the display is only ever being updated by one request. If the service is called whilst the display is being updated, it will wait retry 5 times within the space of a second and if the display is still being updated, the request will be cancelled to ensure the display cursor position is not broken.
    
    This may mean that some requests jump the queue if there is unfortunate timing, but realistically the user should never be pressing buttons that fast in the first place.

--- src/client/display_service.py
+++ src/client/display_service.py
@@ -1,3 +1,5 @@
+import time
+
 import liquidcrystal_i2c
 from datetime import datetime
 
@@ -8,8 +10,18 @@ rows = 4
 class DisplayService():
     def __init__(self):
         self.__lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
+        self.changing_screen = False
 
     def __set_lcd(self, lines, centre_align=True):
+        count = 0
+        while self.changing_screen:
+            time.sleep(0.2)
+            count += 1
+            if count >= 5:
+                return
+
+        self.changing_screen = True
+
         if len(lines) > 4:
             lines = lines[:3]
 
@@ -33,6 +45,8 @@ class DisplayService():
             self.__lcd.printline(1, lines[1].rjust(cols))
             self.__lcd.printline(2, lines[2].rjust(cols))
             self.__lcd.printline(3, lines[3].rjust(cols))
+
+        self.changing_screen = False
 
     def display_message(self, message):
         self.__set_lcd(message)
