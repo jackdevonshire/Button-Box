7fce86a8fc1e574e64e1aeab9ce78d1a41b81175
Jack Devonshire <itunes.jd@hotmail.com>
2024-07-20 09:57:53 +0100

    Speeding up display update times
    The display is quite slow to update when constantly sending update messages. Now we only update the lines that have changed.
    
    In the future I could make it so that it only updates the individual line segments that have changed through manipulating the screen cursor, but right now that would be overkill.

--- src/client/display_service.py
+++ src/client/display_service.py
@@ -11,8 +11,12 @@ class DisplayService():
     def __init__(self):
         self.__lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
         self.changing_screen = False
+        self.line_one = ""
+        self.line_two = ""
+        self.line_three = ""
+        self.line_four = ""
 
-    def __set_lcd(self, lines, centre_align=True):
+    def __set_lcd(self, lines):
         count = 0
         while self.changing_screen:
             time.sleep(0.2)
@@ -35,16 +39,16 @@ class DisplayService():
                 line = line[:19]
             lines.append(line)
 
-        if centre_align:
+        if self.line_one != lines[0]:
             self.__lcd.printline(0, lines[0].center(cols))
+        if self.line_two != lines[1]:
             self.__lcd.printline(1, lines[1].center(cols))
+        if self.line_three != lines[2]:
             self.__lcd.printline(2, lines[2].center(cols))
+        if self.line_four != lines[3]:
             self.__lcd.printline(3, lines[3].center(cols))
-        else:
-            self.__lcd.printline(0, lines[0].rjust(cols))
-            self.__lcd.printline(1, lines[1].rjust(cols))
-            self.__lcd.printline(2, lines[2].rjust(cols))
-            self.__lcd.printline(3, lines[3].rjust(cols))
+
+        self.line_one, self.line_two, self.line_three, self.line_four = lines[0], lines[1], lines[2], lines[3]
 
         self.changing_screen = False
 
