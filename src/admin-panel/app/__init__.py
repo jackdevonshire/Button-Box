1c02af1032b80e2b8cd5973d0d7d2c5c3f14544f
Jack Devonshire <itunes.jd@hotmail.com>
2025-01-12 13:40:50 +0000

    Moving setting setup into init

--- src/admin-panel/app/__init__.py
+++ src/admin-panel/app/__init__.py
@@ -28,7 +28,6 @@ for integration in all_integrations:
     app.register_blueprint(integration.blueprint)
     print(f"Blueprint for ({integration.name}) successfully loaded")
 
-
 # Create the database and tables
 with app.app_context():
     db.create_all()
@@ -37,3 +36,19 @@ with app.app_context():
 for integration in all_integrations:
     integration.initialise()
     print(f"Integration ({integration.name}) successfully initialised")
+
+# Initialise default settings
+from app.core.models import Setting
+with app.app_context():
+    settings = Setting.query.all()
+    if len(settings) < 3:
+        print("Invalid settings detected. Populating default setting values")
+
+        ip_setting = Setting(key="ButtonBoxIP", value="", visible=True)
+        default_configuration = Setting(key="DefaultConfigurationId", value="", visible=False)
+        current_configuration = Setting(key="CurrentConfigurationId", value="", visible=False)
+
+        db.session.add(ip_setting)
+        db.session.add(default_configuration)
+        db.session.add(current_configuration)
+        db.session.commit()
--- src/admin-panel/app/core/core_service.py
+++ src/admin-panel/app/core/core_service.py
@@ -6,19 +6,3 @@ from app import app
 class CoreService:
     def __init__(self, db: SQLAlchemy):
         self.db = db
-        self.initalise()
-
-    def initalise(self):
-        with app.app_context():
-            settings = Setting.query.all()
-            if len(settings) < 3:
-                print("No settings detected. Populating default setting values")
-
-                ip_setting = Setting(key="ButtonBoxIP", value="", visible=True)
-                default_configuration = Setting(key="DefaultConfigurationId", value="", visible=False)
-                current_configuration = Setting(key="CurrentConfigurationId", value="", visible=False)
-
-                self.db.session.add(ip_setting)
-                self.db.session.add(default_configuration)
-                self.db.session.add(current_configuration)
-                self.db.session.commit()
