7b30f6b0148a1d9f05bbd95ce8f7b263687f6544
Jack Devonshire <itunes.jd@hotmail.com>
2025-01-12 12:44:33 +0000

    Update controllers.py

--- src/admin-panel/app/integrations/keyboard/controllers.py
+++ src/admin-panel/app/integrations/keyboard/controllers.py
@@ -6,7 +6,7 @@ bp_keyboard = Blueprint('bp_keyboard', __name__, url_prefix='/integration/keyboa
 
 # Standard Routes (the same across all integrations
 @bp_keyboard.route("/configure")
-def hello():
+def configure():
     return "Hello World", 200 # TODO - Will return a page in the future specifically for configuring this integration
 
 # None standard routes - Any API routes specific for THIS integration
