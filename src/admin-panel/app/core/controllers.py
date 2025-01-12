6f21f0fd84da2e750e2ab74f7783c8c73fb98db2
Jack Devonshire <itunes.jd@hotmail.com>
2025-01-12 14:46:05 +0000

    Started to add dashboard HTML

--- src/admin-panel/app/core/controllers.py
+++ src/admin-panel/app/core/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from app.core.core_service import CoreService
 from app.core.display_service import DisplayService
 from app.core.key_service import KeyService
@@ -8,9 +8,6 @@ core = Blueprint('core', __name__, url_prefix='')
 core_service = CoreService(db)
 key_service = KeyService(db)
 
-# POST /event - To get button events from Button Box
-# GET /integrations - To get a list of all integrations
-# GET /co
-
-# GET /configuraton - To get all configurations, with the active one marked with an "Active" flag
-# POST /
+@core.route("/", methods=["GET"])
+def dashboard():
+    return render_template("core/dashboard.html")
