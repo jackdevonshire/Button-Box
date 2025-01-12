c174de05743c3b3c7e45931f5b2c0907fa75f156
Jack Devonshire <itunes.jd@hotmail.com>
2025-01-12 16:06:56 +0000

    Adding class to handle network responses so we can pass errors to frontend nicely

--- src/admin-panel/app/core/types.py
+++ src/admin-panel/app/core/types.py
@@ -1,4 +1,51 @@
-from enum import Enum
+from enum import Enum, IntEnum
+from flask import jsonify, make_response
+
+
+class HttpStatusCode(IntEnum):
+    Success = 200
+    BadRequest = 400
+    Unauthorized = 401
+    Forbidden = 403
+    NotFound = 404
+    Conflict = 409
+    InternalServerError = 500
+
+
+class NetworkResponse:
+    has_error = False
+    message = ""
+    data = {}
+    status_code = 200
+
+    def with_data(self, data):
+        self.data = data
+        return self
+
+    def with_error(self, message):
+        self.has_error = True
+        self.message = message
+        return self
+
+    def with_status_code(self, code: HttpStatusCode):
+        self.status_code = code.value
+        return self
+
+    def get(self):
+        if self.has_error and self.status_code == 200:
+            self.status_code = 500
+
+        response = {
+            "HasError": self.has_error,
+            "Message": self.message
+        }
+
+        for key, value in self.data.items():
+            response[key] = value
+
+        flask_response = make_response(jsonify(response))
+        flask_response.status_code = self.status_code
+        return flask_response
 
 
 class EventType(Enum):
